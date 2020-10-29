8f2d607dd2015f4e7c67ba56b36fa0ab2a44262b
yuhaiyang <yyusea@gmail.com>
2020-10-29 20:04:54 +0800

    get mingw-w64-x86_64-libusb-1.0.23-1-any.pkg.tar.xz

--- CI/appveyor/install_msys_deps.sh
+++ CI/appveyor/install_msys_deps.sh
@@ -6,8 +6,13 @@ SCOPY_MINGW_BUILD_DEPS_BRANCH=master
 echo "Download and install pre-compiled libraries ... "
 wget "https://ci.appveyor.com/api/projects/$SCOPY_MINGW_BUILD_DEPS_FORK/scopy-mingw-build-deps/artifacts/scopy-$MINGW_VERSION-build-deps-pacman.txt?branch=$SCOPY_MINGW_BUILD_DEPS_BRANCH&job=Environment: MINGW_VERSION=$MINGW_VERSION, ARCH=$ARCH" -O /tmp/scopy-$MINGW_VERSION-build-deps-pacman.txt
 wget "https://ci.appveyor.com/api/projects/$SCOPY_MINGW_BUILD_DEPS_FORK/scopy-mingw-build-deps/artifacts/scopy-$MINGW_VERSION-build-deps.tar.xz?branch=$SCOPY_MINGW_BUILD_DEPS_BRANCH&job=Environment: MINGW_VERSION=$MINGW_VERSION, ARCH=$ARCH" -O /tmp/scopy-$MINGW_VERSION-build-deps.tar.xz
+
+cd /c/old_msys_deps_mingw64
+wget "https://repo.msys2.org/mingw/x86_64/mingw-w64-x86_64-libusb-1.0.23-1-any.pkg.tar.xz"
+
 cd /c
 tar xJf /tmp/scopy-$MINGW_VERSION-build-deps.tar.xz
+
 
 SCOPY_MINGW_BUILD_DEPS_PACMAN=$(</tmp/scopy-$MINGW_VERSION-build-deps-pacman.txt)
 PACMAN_SYNC_DEPS="
